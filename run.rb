1e1cb6d1e828a7eb6e3d988560e7497c05558f54
Felipe Guerço Oliveira <felipe.oliveira@m4u.com.br>
2019-09-05 14:02:00 -0300

    Added parameter to filter or exclude single days

--- run.rb
+++ run.rb
@@ -6,6 +6,24 @@ require_relative 'days'
 
 year = ARGV[0].to_i
 month = ARGV[1].to_i
+extras = ARGV[2]
+
+puts extras
+
+only = []
+except = []
+
+unless extras.nil?
+  sep = extras.split('=')
+  rule = sep[0]
+  data = sep[1]
+
+  if rule == 'only'
+    only.push(*data.split(','))
+  elsif rule == 'except'
+    except.push(*data.split(','))
+  end
+end
 
 issues_worked = my_issues(year, month)
 work_days = work_days(year, month)
@@ -16,32 +34,33 @@ HOURS_PER_DAY = 8
 HOURS_PER_ISSUE = (HOURS_PER_DAY / issues_per_day).ceil
 STARTING_HOUR = 9
 
-puts "issues: #{issues_worked.size}"
+puts "issues to log: #{issues_worked.size}"
 puts "work days: #{work_days.size}"
-puts "hour per issue: #{HOURS_PER_ISSUE}"
+puts "hours per issue: #{HOURS_PER_ISSUE}"
 
 issues_to_log = issues_worked
-.map do |i|
-  {
-    issue: i,
-    hours_left: HOURS_PER_ISSUE
-  }
-end
-
-puts issues_to_log
+  .map do |i|
+    {
+      issue: i,
+      hours_left: HOURS_PER_ISSUE
+    }
+  end
 
 next_issue = issues_to_log.shift
 
-work_days.each do |d|
-  puts "logging work for #{d}"
+filtered_days =
+  work_days
+  .reject { |d| except.include?(d.day.to_s)  }
+  .select { |d| only.empty? || only.include?(d.day.to_s) }
+
+filtered_days.each do |d|
+  puts "\n", '-' * 80, "Logging work for #{d}:"
 
   minute = 0
   logged = 0
 
   while logged < HOURS_PER_DAY
     pending = HOURS_PER_DAY - logged
-
-    puts "  > next issue: #{next_issue}"
 
     hours = next_issue[:hours_left]
     hours = pending if hours > pending
@@ -51,8 +70,6 @@ work_days.each do |d|
     date_log = Time.new(d.year, d.month, d.day, STARTING_HOUR + logged, minute)
     minute += 10
 
-    puts "    > log_work('#{next_issue[:issue]}', '#{date_log.to_s}', '#{hours}h')"
-    # log work here
     log_work(next_issue[:issue], date_log.to_s, "#{hours}h")
     logged += hours
 
